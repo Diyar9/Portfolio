839a636a9cd2b3e2baf98174d71e84c378cce7ba
Diyar9 <diyar.d@hotmail.com>
2023-09-05 18:21:06 +0200

    transistion fixed

--- src/components/PageThree.jsx
+++ src/components/PageThree.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './PageThree.css';
+import './ScrollAnimation.css';
 import { Helmet } from 'react-helmet-async';
 import ScrollAnimation from './ScrollAnimation';
 
 const PageThree = () => {
-
+    <ScrollAnimation />
     return (
         <div className="page3">
             <Helmet>
@@ -12,15 +13,14 @@ const PageThree = () => {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai+Looped:wght@700&display=swap" rel="stylesheet" />
             </Helmet>
-            <ScrollAnimation />
-            <div className="experience-title-box fade-in scroll-in">
+            <div className="experience-title-box">
                 <div className="experience-title-column"><h2>Experience</h2></div>
             </div>
             <div className="experience-box">
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-left in-viewport">
                     <img src=".\ExperienceIcon\orebro_01.png" alt="Örebro University" />
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-right in-viewport">
                     <div className="experience-column-text"><h2>2021 Janurary</h2></div>
                     <div className="experience-column-text"><h3>I started studying <span className='highlight-text'>systems science</span> at Örebro University. </h3></div>
                     <div className="experience-column-text" id='courses'>
@@ -39,25 +39,25 @@ const PageThree = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-left in-viewport">
                     <div className="experience-column-text"><h2>2023 Janurary - May</h2></div>
                     <div className="experience-column-text"><h3>Internship at Combitech AB as a <span className='highlight-text'>system developer</span> in Gothenburg.</h3></div>
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-right in-viewport">
                     <img src=".\ExperienceIcon\Combitech.jpg" alt="Combitech" />
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-left in-viewport">
                     <img src=".\ExperienceIcon\sileon.jpg" alt="Sileon" />
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-right in-viewport">
                     <div className="experience-column-text"><h2>2023 June - July</h2></div>
                     <div className="experience-column-text"><h3>Summer internship at Sileon AB as an <span className='highlight-text'>ambassador</span>.</h3></div>
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-left in-viewport">
                     <div className="experience-column-text"><h2>2024 Janurary</h2></div>
                     <div className="experience-column-text"><h3><span className='highlight-text'>Bachelor’s degree</span> in systems science.</h3></div>
                 </div>
-                <div className="experience-column fade-in scroll-in" >
+                <div className="experience-column fade-in scroll-in-right in-viewport">
                     <img src=".\ExperienceIcon\examen2.png" alt="Graduated" />
                 </div>
                 <div className="experience-line"></div>
